50157005224c334013a4eb75f34d197a519b2c96
gio sulakadze <80586641+Sula000@users.noreply.github.com>
2023-03-31 21:36:54 +0400

    js

---
+++ script.js
@@ -0,0 +1,31 @@
+
+let mobileMenu = document.querySelector(".header_nav");
+let burger = document.querySelector(".menu_bar_icon");
+let close = document.querySelector(".close_icon");
+
+burger.addEventListener("click", () => {
+    mobileMenu.classList.add("gaqroba_gamochena");
+    // burger.style.display = "none";
+
+});
+close.addEventListener("click", () => {
+
+    mobileMenu.classList.remove("gaqroba_gamochena")
+
+});
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
